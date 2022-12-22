430fd98a0f3ed1c7198dcee7872be11c3f608d76
Ifiok Udoidiok <ifiok@fleetrover.com>
2022-12-22 14:41:03 +0100

    Write Function to initialize websocket

--- sockets/index.ts
+++ sockets/index.ts
@@ -2,7 +2,7 @@ import io from 'socket.io-client';
 import appStore from '../store/AppStore';
 
 export const initSocket = () => {
-  const SOCKET_URL = `${process.env.NEXT_PUBLIC_RCF_APP_URL}/${process.env.NEXT_PUBLIC_SOCKETIO_NAMESPACE}`;
+  const SOCKET_URL = `${process.env.NEXT_PUBLIC_RCF_APP_URL}:5056`;
   const socket = io(SOCKET_URL);
   // Wait for socket to be connected to save in store.
   socket.on('connect', () => {
