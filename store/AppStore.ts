3210681dfc9252037a1c87543d845b3474b07753
Ifiok Udoidiok <ifiok@fleetrover.com>
2022-12-22 14:38:13 +0100

    Add objects to store

--- store/AppStore.ts
+++ store/AppStore.ts
@@ -6,14 +6,20 @@ import { IMessage } from 'interfaces/messages';
 interface IAppStore {
   socket: Socket | Record<string, any>;
   user: IUser | null;
+  token: string | null,
+  room: string;
   message: IMessage | null;
+  messagesRecieved: IMessage | null;
   reset: () => void;
 }
 
 const storeDefaults = {
   socket: {},
   user: null,
+  token: null,
+  room: null,
   message: null,
+  messagesRecieved: null
 };
 
 const appStore = create(
